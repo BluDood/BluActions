ecee6123730bdad4adb17934346134bba1897985
BluDood <bludood@bludood.com>
2026-03-16 23:08:55 +0100

    fix: add back empty configs to fix build

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  main: {},
+  preload: {},
   renderer: {
     resolve: {
       alias: {
